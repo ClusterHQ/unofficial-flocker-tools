2cffb9a9326bb673cff013505ab52d3cec0f693e
Luke Marsden <luke@clusterhq.com>
2015-05-27 14:43:09 +0000

    initial commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+import os
+import yaml
+
+print "This script will generate some certificates using flocker-ca and"
+print "upload them to the servers specified in a cluster.yml"
+
+
