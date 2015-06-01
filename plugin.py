64e60e8dbd47e44f81c28d8e1ce3cc3baec0ba3b
Kai Davenport <kaiyadavenport@gmail.com>
2015-06-01 17:26:18 +0100

    once the plugin certs are generated - upload them into /etc/flocker/plugin.crt /etc/flocker/plugin.key

--- plugin.py
+++ plugin.py
@@ -14,6 +14,11 @@ from utils import Configurator
 if __name__ == "__main__":
     c = Configurator(configFile=sys.argv[1])
     control_ip = c.config["control_node"]
+    print "Generating plugin certs"
     for node in c.config["agent_nodes"]:
         c.run("flocker-ca create-api-certificate %s" % (node + '-plugin',))
-        print "Created plugin user key for", node
+        print "Generated plugin certs for", node
+        for ext in ("crt", "key"):
+            c.scp("%s-plugin.%s" % (node, ext),
+                node, "/etc/flocker/plugin.%s" % (ext,))
+        print "Uploaded plugin certs for", node
