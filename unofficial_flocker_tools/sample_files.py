9fbf3de24dbd48f9305d5b6b53b495ff1738bbd8
Rob <me@robhaswell.co.uk>
2015-06-18 16:30:32 +0100

    flocker-sample-files

---
+++ unofficial_flocker_tools/sample_files.py
@@ -0,0 +1,11 @@
+import shutil
+from pkg_resources import resource_filename
+
+def main():
+    print "Copying the following files to the current working directory:"
+
+    for backend in ["ebs", "openstack", "zfs"]:
+        filename = "cluster.yml.%s.sample" % (backend,)
+        resource = resource_filename("unofficial_flocker_tools", "samples/" + filename)
+        shutil.copyfile(resource, filename)
+        print filename
