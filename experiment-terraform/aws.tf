da98206274a9ca6d74484b9f585d5d888cdcf5db
Luke Marsden <luke@clusterhq.com>
2015-09-15 10:06:27 +0100

    aws security groups

--- experiment-terraform/aws.tf
+++ experiment-terraform/aws.tf
@@ -3,14 +3,55 @@ provider "aws" {
     secret_key = "${var.aws_secret_key}"
     region = "${var.aws_region}"
 }
+provider "aws_security_group" "cluster_security_group" {
+  name = "flocker_rules"
+  description = "Allow SSH, HTTP, Flocker APIs"
+  # ssh
+  ingress {
+      from_port = 22
+      to_port = 22
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+  }
+  # http for demo
+  ingress {
+      from_port = 80
+      to_port = 80
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+  }
+  # external flocker api
+  ingress {
+      from_port = 4523
+      to_port = 4523
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+  }
+  # internal flocker-control port
+  ingress {
+      from_port = 4524
+      to_port = 4524
+      protocol = "tcp"
+      self = true
+  }
+  # allow outbound traffic
+  egress {
+      from_port = 0
+      to_port = 0
+      protocol = "-1"
+      cidr_blocks = ["0.0.0.0/0"]
+  }
+}
 resource "aws_instance" "master" {
     ami = "${lookup(var.aws_ubuntu_amis, var.aws_region)}"
     instance_type = "${var.aws_instance_type}"
     availability_zone = "${var.aws_availability_zone}"
+    security_groups = [flocker_rules]
 }
 resource "aws_instance" "nodes" {
     ami = "${lookup(var.aws_ubuntu_amis, var.aws_region)}"
     instance_type = "${var.aws_instance_type}"
     availability_zone = "${var.aws_availability_zone}"
     count = 3
+    security_groups = [flocker_rules]
 }
