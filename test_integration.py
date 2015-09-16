1f8bae3d915fcdcbdace3c9c60da7650557918ea
Luke Marsden <luke@clusterhq.com>
2015-09-16 10:34:55 +0100

    just give me raw ssh

--- test_integration.py
+++ test_integration.py
@@ -17,8 +17,7 @@ luke@tiny:~$ cat ~/terraform.tfvars.json
 
 from twisted.trial.unittest import TestCase
 import os
-from pipes import quote as shell_quote
-from subprocess import PIPE, Popen
+from subprocess import check_output
 from twisted.python.filepath import FilePath
 import yaml, json
 
@@ -59,10 +58,10 @@ class UnofficialFlockerInstallerTests(TestCase):
             self.assertNotEqual(node1, node2)
             node1public = node1['public']
             node2public = node2['public']
-            print run(node1public, [
+            print runSSHRaw(node1public, [
                 'docker run -v foo:/data --volume-driver=flocker busybox '
                 'sh -c \\"echo hello \\> /data/foo\\"'])
-            output = run(node2public, [
+            output = runSSHRaw(node2public, [
                 'docker run -v foo:/data --volume-driver=flocker busybox '
                 'cat /data/foo'])
             self.assertTrue(output.strip().endswith("hello"))
@@ -77,69 +76,7 @@ class UnofficialFlockerInstallerTests(TestCase):
     def test_ubuntu_aws(self):
         return self._run_integration_test("ubuntu-aws")
 
-
-def run(node, command, input=""):
-    """
-    Synchronously run a command (list of bytes) on a node's address (bytes)
-    with optional input (bytes).
-    """
-    print "Running", command, "on", node
-    result = run_SSH(22, "root", node, command, input, key=KEY)
-    print "Output from", node + ":", result, "(%s)" % (command,)
-    return result
-
-
-def run_SSH(port, user, node, command, input, key=None,
-            background=False):
-    """
-    Run a command via SSH.
-
-    :param int port: Port to connect to.
-    :param bytes user: User to run the command as.
-    :param bytes node: Node to run command on.
-    :param command: Command to run.
-    :type command: ``list`` of ``bytes``.
-    :param bytes input: Input to send to command.
-    :param FilePath key: If not None, the path to a private key to use.
-    :param background: If ``True``, don't block waiting for SSH process to
-         end or read its stdout. I.e. it will run "in the background".
-         Also ensures remote process has pseudo-tty so killing the local SSH
-         process will kill the remote one.
-
-    :return: stdout as ``bytes`` if ``background`` is false, otherwise
-        return the ``subprocess.Process`` object.
-    """
-    quotedCommand = ' '.join(map(shell_quote, command))
-    command = [
-        b'ssh',
-        b'-p', b'%d' % (port,),
-        b'-o', b'StrictHostKeyChecking=no',
-        b'-o', b'UserKnownHostsFile=/dev/null',
-        ]
-
-    if key is not None:
-        command.extend([
-            b"-i",
-            key.path])
-
-    if background:
-        # Force pseudo-tty so that remote process exists when the ssh
-        # client does:
-        command.extend([b"-t", b"-t"])
-
-    command.extend([
-        b'@'.join([user, node]),
-        quotedCommand
-    ])
-    if background:
-        process = Popen(command, stdin=PIPE)
-        process.stdin.write(input)
-        return process
-    else:
-        process = Popen(command, stdout=PIPE, stdin=PIPE, stderr=PIPE)
-
-    result = process.communicate(input)
-    if process.returncode != 0:
-        raise Exception('Command Failed', command, process.returncode, result)
-
-    return result[0]
+def runSSHRaw(ip, command, username="root"):
+    command = 'ssh -o LogLevel=error -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i %s %s@%s %s' % (
+            KEY.path, username, ip, command)
+    return check_output(command, shell=True)
