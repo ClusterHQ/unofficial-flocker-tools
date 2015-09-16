954df2834eb7c069cc8cd74f6beb301c8f7d2cc4
Luke Marsden <luke@clusterhq.com>
2015-09-16 23:49:42 +0100

    passthrough option for forcing destruction of infrastructure (rather than asking)

--- unofficial_flocker_tools/destroy_nodes.py
+++ unofficial_flocker_tools/destroy_nodes.py
@@ -8,7 +8,8 @@ def main():
     if not terraform_templates.exists():
         print "Please run uft-flocker-sample-files in the current directory first."
         os._exit(1)
-    os.system("cd terraform && terraform destroy -force")
+    os.system("cd terraform && terraform destroy"
+            + (" -force" if "FORCE_DESTROY" in os.environ else ""))
     pass
 
 if __name__ == "__main__":
