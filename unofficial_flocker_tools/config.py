45d8062dfa4448246f2798ade463f12a8ec46498
Luke Marsden <luke@clusterhq.com>
2015-09-20 16:54:41 +0100

    attempt to improve ||ism

--- unofficial_flocker_tools/config.py
+++ unofficial_flocker_tools/config.py
@@ -6,11 +6,16 @@
 import sys
 import yaml
 from twisted.internet.task import react
-from twisted.internet.defer import succeed, inlineCallbacks
+from twisted.internet.defer import inlineCallbacks, gatherResults
 
 # Usage: deploy.py cluster.yml
 from utils import Configurator
 
+def report_completion(result, public_ip, message="Completed install for"):
+    print message, public_ip
+    return result
+
+@inlineCallbacks
 def main(reactor, args):
     c = Configurator(configFile=sys.argv[1])
     c.run("flocker-ca initialize %s" % (c.config["cluster_name"],))
@@ -27,16 +32,6 @@ def main(reactor, args):
     for user in c.config["users"]:
         c.run("flocker-ca create-api-certificate %s" % (user,))
         print "Created user key for", user
-    print "Uploading keys to respective nodes:"
-
-    # Copy cluster cert, and control cert and key to control node.
-    c.runSSHRaw(c.config["control_node"], "mkdir -p /etc/flocker")
-    c.scp("cluster.crt", c.config["control_node"], "/etc/flocker/cluster.crt")
-    print " * Uploaded cluster cert to control node."
-    for ext in ("crt", "key"):
-        c.scp("control-%s.%s" % (c.config["control_node"], ext),
-                c.config["control_node"], "/etc/flocker/control-service.%s" % (ext,))
-    print " * Uploaded control cert & key to control node."
 
     # Dump agent_config into a file and scp it to /etc/flocker/agent.yml on the
     # nodes.
@@ -49,31 +44,56 @@ def main(reactor, args):
     yaml.dump(node_mapping, f)
     f.close()
 
+    print "Uploading keys to respective nodes:"
+    deferreds = []
+
+    # Copy cluster cert, and control cert and key to control node.
+    d = c.runSSHAsync(c.config["control_node"], "mkdir -p /etc/flocker")
+    d.addCallback(lambda ignored: c.scp("cluster.crt", c.config["control_node"], "/etc/flocker/cluster.crt", async=True))
+    d.addCallback(report_completion, public_ip=c.config["control_node"], message=" * Uploaded cluster cert to")
+    deferreds.append(d)
+
+    for ext in ("crt", "key"):
+        d = c.scp("control-%s.%s" % (c.config["control_node"], ext),
+                c.config["control_node"], "/etc/flocker/control-service.%s" % (ext,), async=True)
+        d.addCallback(report_completion, public_ip=c.config["control_node"], message=" * Uploaded control %s to" % (ext,))
+        deferreds.append(d)
+    print " * Uploaded control cert & key to control node."
+
     # Copy cluster cert, and agent cert and key to agent nodes.
     for node, uuid in node_mapping.iteritems():
-        c.runSSHRaw(node, "mkdir -p /etc/flocker")
-        c.scp("cluster.crt", node, "/etc/flocker/cluster.crt")
-        c.scp("agent.yml", node, "/etc/flocker/agent.yml")
-        print " * Uploaded cluster cert to %s." % (node,)
-        for ext in ("crt", "key"):
-            c.scp("%s.%s" % (uuid, ext), node, "/etc/flocker/node.%s" % (ext,))
-        print " * Uploaded node cert and key to %s." % (node,)
+        d = c.runSSHAsync(node, "mkdir -p /etc/flocker")
+        d.addCallback(lambda ignored: c.scp("cluster.crt", node, "/etc/flocker/cluster.crt", async=True))
+        d.addCallback(report_completion, public_ip=node, message=" * Uploaded cluster cert to")
+        deferreds.append(d)
 
+        d = c.scp("agent.yml", node, "/etc/flocker/agent.yml", async=True)
+        d.addCallback(report_completion, public_ip=node, message=" * Uploaded agent.yml to")
+        deferreds.append(d)
+
+        for ext in ("crt", "key"):
+            d = c.scp("%s.%s" % (uuid, ext), node, "/etc/flocker/node.%s" % (ext,))
+            d.addCallback(report_completion, public_ip=node, message=" * Uploaded node %s to" % (ext,))
+            deferreds.append(d)
+
+    yield gatherResults(deferreds)
+
+    deferreds = []
     for node, uuid in node_mapping.iteritems():
         if c.config["os"] == "ubuntu":
-            print c.runSSH(node, """echo "starting flocker-container-agent..."
-service flocker-container-agent restart
+            d = c.runSSHAsync(node, """echo "starting flocker-container-agent..."
+service flocker-container-agent start
 echo "starting flocker-dataset-agent..."
-service flocker-dataset-agent restart
+service flocker-dataset-agent start
 """)
         elif c.config["os"] == "centos":
-            print c.runSSH(node, """if selinuxenabled; then setenforce 0; fi
+            d = c.runSSHAsync(node, """if selinuxenabled; then setenforce 0; fi
 systemctl enable docker.service
 systemctl start docker.service
 """)
 
         elif c.config["os"] == "coreos":
-            c.runSSH(node, """echo
+            d = c.runSSHAsync(node, """echo
 echo > /tmp/flocker-command-log
 docker run --restart=always -d --net=host --privileged \\
     -v /etc/flocker:/etc/flocker \\
@@ -88,9 +108,11 @@ docker run --restart=always -d --net=host --privileged \\
     --name=flocker-dataset-agent \\
     clusterhq/flocker-dataset-agent
 """)
+        deferreds.append(d)
+    yield gatherResults(deferreds)
 
     if c.config["os"] == "ubuntu":
-        c.runSSH(c.config["control_node"], """cat <<EOF > /etc/init/flocker-control.override
+        d = c.runSSHAsync(c.config["control_node"], """cat <<EOF > /etc/init/flocker-control.override
 start on runlevel [2345]
 stop on runlevel [016]
 EOF
@@ -101,7 +123,7 @@ ufw allow flocker-control-api
 ufw allow flocker-control-agent
 """)
     elif c.config["os"] == "centos":
-        c.runSSH(c.config["control_node"], """systemctl enable flocker-control
+        d = c.runSSHAsync(c.config["control_node"], """systemctl enable flocker-control
 systemctl start flocker-control
 firewall-cmd --permanent --add-service flocker-control-api
 firewall-cmd --add-service flocker-control-api
@@ -109,20 +131,11 @@ firewall-cmd --permanent --add-service flocker-control-agent
 firewall-cmd --add-service flocker-control-agent
 """)
     elif c.config["os"] == "coreos":
-        c.runSSH(c.config["control_node"], """echo
+        d = c.runSSHAsync(c.config["control_node"], """echo
 docker run --name=flocker-control-volume -v /var/lib/flocker clusterhq/flocker-control-service true
 docker run --restart=always -d --net=host -v /etc/flocker:/etc/flocker --volumes-from=flocker-control-volume --name=flocker-control-service clusterhq/flocker-control-service""")
 
-    print "Configured and started control service, opened firewall."
-
-    if c.config["users"]:
-        print "\nYou should now be able to communicate with the control service, for example:\n"
-        prefix = ("curl -s --cacert $PWD/cluster.crt --cert $PWD/%(user)s.crt "
-                  "--key $PWD/%(user)s.key" % dict(user=c.config["users"][0],))
-        url = "https://%(control_node)s:4523/v1" % dict(control_node=c.config["control_node"],)
-        print "This should give you a list of your nodes:"
-        print prefix + " " + url + "/state/nodes | jq ."
-    return succeed(None)
+    yield d
 
 def _main():
     react(main, sys.argv[1:])
