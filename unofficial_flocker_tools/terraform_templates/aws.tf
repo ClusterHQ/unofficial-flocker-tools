ccc1e6ee12e3a5ee4ef04cf28cfdab0a181df561
Luke Marsden <luke@clusterhq.com>
2015-09-23 11:24:01 +0100

    Add some user friendly tags

--- unofficial_flocker_tools/terraform_templates/aws.tf
+++ unofficial_flocker_tools/terraform_templates/aws.tf
@@ -19,12 +19,18 @@ resource "aws_vpc" "cluster_vpc" {
     cidr_block = "10.0.0.0/16"
     enable_dns_support = "true"
     enable_dns_hostnames = "true"
+    tags {
+        Name = "Flocker VPC"
+    }
 }
 resource "aws_subnet" "cluster_subnet" {
     vpc_id = "${aws_vpc.cluster_vpc.id}"
     cidr_block = "10.0.0.0/16"
     availability_zone = "${var.aws_availability_zone}"
     map_public_ip_on_launch = "true"
+    tags {
+        Name = "Flocker subnet"
+    }
 }
 resource "aws_internet_gateway" "gateway" {
     vpc_id = "${aws_vpc.cluster_vpc.id}"
