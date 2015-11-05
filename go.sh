4dd97901c65cfd4c0149215e9dc0de1a4dfb822a
Luke Marsden <luke@clusterhq.com>
2015-11-05 10:14:40 -0800

    Make flockerctl a valid alias for flocker-volumes.

--- go.sh
+++ go.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 IMAGE="clusterhq/uft:latest"
-for CMD in flocker-ca flocker-deploy flocker-config flocker-install flocker-plugin-install flocker-sample-files flocker-tutorial flocker-volumes flocker-get-nodes flocker-destroy-nodes; do
+for CMD in flockerctl flocker-ca flocker-deploy flocker-config flocker-install flocker-plugin-install flocker-sample-files flocker-tutorial flocker-volumes flocker-get-nodes flocker-destroy-nodes; do
     cat <<EOF |sudo tee /usr/local/bin/uft-$CMD >/dev/null
 #!/bin/sh
 docker run -ti --rm -e CUSTOM_REPO=\${CUSTOM_REPO} -e FORCE_DESTROY=\${FORCE_DESTROY} -e CONTAINERIZED=1 -v /:/host -v \$PWD:/pwd $IMAGE $CMD "\$@"
