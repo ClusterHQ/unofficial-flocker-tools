143b9d6f29ccabdc677a1ae7189163cd00fd8abb
Luke Marsden <luke@clusterhq.com>
2015-11-06 14:36:03 -0800

    rename uft-flockerctl to flockerctl

--- go.sh
+++ go.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
 IMAGE="clusterhq/uft:latest"
 for CMD in flockerctl flocker-ca flocker-deploy flocker-config flocker-install flocker-plugin-install flocker-sample-files flocker-tutorial flocker-volumes flocker-get-nodes flocker-destroy-nodes; do
-    cat <<EOF |sudo tee /usr/local/bin/uft-$CMD >/dev/null
+    if [ "$CMD" = "flockerctl" ]; then
+        PREFIX=""
+    else
+        PREFIX="uft-"
+    fi
+    cat <<EOF |sudo tee /usr/local/bin/${PREFIX}${CMD} >/dev/null
 #!/bin/sh
 docker run -ti --rm -e CUSTOM_REPO=\${CUSTOM_REPO} -e FORCE_DESTROY=\${FORCE_DESTROY} -e CONTAINERIZED=1 -v /:/host -v \$PWD:/pwd $IMAGE $CMD "\$@"
 EOF
-sudo chmod +x /usr/local/bin/uft-$CMD
-echo "Installed /usr/local/bin/uft-$CMD"
+    sudo chmod +x /usr/local/bin/${PREFIX}${CMD}
+    echo "Installed /usr/local/bin/${PREFIX}${CMD}"
 done
 echo "Pulling Docker image for Flocker installer..."
 docker pull $IMAGE
