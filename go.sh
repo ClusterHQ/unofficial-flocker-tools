5d78daa1aee17caeb7503e66073bc6c68f9b1450
Luke Marsden <luke@clusterhq.com>
2015-11-27 15:24:32 +0000

    Add some spacing to make error messages easier to read.

--- go.sh
+++ go.sh
@@ -13,28 +13,38 @@ if docker version >/dev/null 2>&1; then
 elif sudo docker version >/dev/null 2>&1; then
     SUDO_PREFIX="sudo "
 else
+    echo
     echo "Unable to reach docker daemon with or without sudo. Please check that"
     echo "docker is running and that DOCKER_HOST is set correctly."
     echo
     echo "If you use docker-machine (e.g. as part of docker toolbox) then"
     echo "'eval \\\$(docker-machine env default)' or similar may help."
+    echo
     echo "In that case, also make sure your docker machine is running, using"
     echo "e.g. echo 'docker-machine start default'."
+    echo
     exit 1
 fi
 
 if [ ! "\$IGNORE_NETWORK_CHECK" = "1" ]; then
     if ! \$SUDO_PREFIX docker run -ti gliderlabs/alpine wget -q -O /dev/null -T 5 http://check.clusterhq.com/?source=uft
         then
+            echo
             echo "Unable to establish network connectivity from inside a container."
+            echo
             echo "If you see an error message above, that may give you a clue how to fix it."
+            echo
             echo "If you run docker in a VM, restarting the VM often helps, especially if"
             echo "you have changed network (and/or DNS servers) since starting the VM."
+            echo
             echo "If you are using docker-machine (e.g. as part of docker toolbox), you can"
             echo "run the following command (or similar) to do that:"
+            echo
             echo "    docker-machine restart default && eval \\\$(docker-machine env default)"
+            echo
             echo "To ignore this check, and proceed anyway (e.g. if you know you are offline)"
             echo "set IGNORE_NETWORK_CHECK=1"
+            echo
             exit 1
         fi
     fi
