0df9585eb190f7ce0e1d5f66a9c3f45de452b117
Luke Marsden <luke@clusterhq.com>
2015-11-27 14:12:47 +0000

    Make the installer work on CentOS-like things, by passing the "make bind-mounts work" flag to docker run -v.

--- go.sh
+++ go.sh
@@ -8,7 +8,7 @@ for CMD in flockerctl flocker-ca flocker-deploy flocker-config flocker-install f
     fi
     cat <<EOF |sudo tee /usr/local/bin/${PREFIX}${CMD} >/dev/null
 #!/bin/sh
-docker run -ti --rm -e TOKEN="\${TOKEN}" -e CUSTOM_REPO=\${CUSTOM_REPO} -e FORCE_DESTROY=\${FORCE_DESTROY} -e CONTAINERIZED=1 -v /:/host -v \$PWD:/pwd $IMAGE $CMD "\$@"
+docker run -ti --rm -e TOKEN="\${TOKEN}" -e CUSTOM_REPO=\${CUSTOM_REPO} -e FORCE_DESTROY=\${FORCE_DESTROY} -e CONTAINERIZED=1 -v /:/host:z -v \$PWD:/pwd:z $IMAGE $CMD "\$@"
 EOF
     sudo chmod +x /usr/local/bin/${PREFIX}${CMD}
     echo "Installed /usr/local/bin/${PREFIX}${CMD}"
