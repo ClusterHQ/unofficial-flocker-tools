bb8db8ce010879c4a40925c3720962178be84d34
Luke Marsden <luke@clusterhq.com>
2015-12-02 10:59:06 +0000

    Install kubernetes from Google-provided installation scripts

--- unofficial_flocker_tools/kubernetes.py
+++ unofficial_flocker_tools/kubernetes.py
@@ -6,75 +6,33 @@ def install_kubernetes(reactor, configFile):
     if c.config["os"] == "ubuntu":
         # Install kubernetes
         deferreds = []
-        log("Starting etcd...")
-        c.runSSHRaw(c.config["control_node"],
-                "docker run --restart=always --net=host --name=etcd -d "
-                "gcr.io/google_containers/etcd:2.0.9 "
-                "/usr/local/bin/etcd --addr=127.0.0.1:4001 --bind-addr=0.0.0.0:4001 "
-                "--data-dir=/var/etcd/data")
-        log("Started etcd.")
 
-        log("Starting k8s master...")
-        c.runSSHRaw(c.config["control_node"],
-                "docker run --restart=always --net=host --name=hyperkube -d "
-                "-v /var/run/docker.sock:/var/run/docker.sock "
-                "gcr.io/google_containers/hyperkube:v1.1.1 /hyperkube kubelet "
-                "--api_servers=http://localhost:8080 --v=2 --address=0.0.0.0 "
-                "--enable_server --hostname_override=127.0.0.1 "
-                "--config=/etc/kubernetes/manifests")
-        log("Started k8s master.")
-
-        log("Starting k8s service proxy...")
-        c.runSSHRaw(c.config["control_node"],
-                "docker run -d --restart=always --name=service-proxy "
-                "--net=host --privileged gcr.io/google_containers/hyperkube:v1.1.1 "
-                "/hyperkube proxy --master=http://localhost:8080 --v=2")
-        log("Started k8s service proxy")
+        log("Installing Kubernetes master...")
+        d = c.runSSHAsync(
+                c.config["control_node"],
+                # Install kubernetes
+                "K8S_VERSION=1.1.2 $("
+                    "curl -sSL https://raw.githubusercontent.com/kubernetes/kubernetes"
+                    "/master/docs/getting-started-guides/docker-multinode/master.sh |sh"
+                ") "
+                # Install kubectl on the master for convenience for user
+                "&& curl -sSL https://storage.googleapis.com/kubernetes-release/"
+                "release/v1.1.2/bin/linux/amd64/kubectl -o /usr/local/bin/kubectl && "
+                "chmod +x /usr/local/bin/kubectl"
+        )
+        d.addCallback(lambda ignored: log("Installed master."))
+        deferreds.append(d)
 
         for node in c.config["agent_nodes"]:
+            log("Installing Kubernetes worker on %s..." % (node['public'],))
             d = c.runSSHAsync(
-                node['public'],
-                "docker -H unix:///var/run/docker-bootstrap.sock run -d "
-                "--restart=always --net=host --privileged -v /dev/net:/dev/net "
-                " --name=flannel quay.io/coreos/flannel:0.5.0 /opt/bin/flanneld "
-                "--etcd-endpoints=http://%(master_address)s:4001" %
-                dict(
-                    master_address=c.config["control_node"],
-                )
+                    node['public'],
+                    "K8S_VERSION=1.1.2 $("
+                        "curl -sSL https://raw.githubusercontent.com/kubernetes/kubernetes"
+                        "/master/docs/getting-started-guides/docker-multinode/master.sh |sh"
+                    ") "
             )
-            d.addCallback(lambda ignored:
-                    log("Started Flannel on %s" % (node['public'],)))
-
-            d.addCallback(lambda ignored:
-                    c.runSSHAsync(
-                        node['public'],
-                        """eval $(docker -H unix:///var/run/docker-bootstrap.sock
-                        exec flannel cat /run/flannel/subnet.env) &&
-                        sed -i s/XXX/--bip=${FLANNEL_SUBNET} \
-                                --mtu=${FLANNEL_MTU}/ /etc/docker/default &&
-                        service docker restart
-                        /sbin/ifconfig docker0 down &&
-                        brctl delbr docker0 &&
-                        docker run --volume=/:/rootfs:ro \
-                                   --volume=/sys:/sys:ro \
-                                   --volume=/dev:/dev \
-                                   --volume=/var/lib/docker/:/var/lib/docker:rw \
-                                   --volume=/var/lib/kubelet/:/var/lib/kubelet:rw \
-                                   --volume=/var/run:/var/run:rw \
-                                   --net=host \
-                                   --privileged=true \
-                                   --pid=host \
-                                   --restart=always \
-                                   -d \
-                                   gcr.io/google_containers/hyperkube:v1.0.1
-                                   /hyperkube kubelet
-                                       --api-servers=http://${MASTER_IP}:8080 --v=2
-                                       --address=0.0.0.0 --enable-server
-                                       --hostname-override=$(hostname -i)
-                                       --cluster-dns=10.0.0.10
-                                       --cluster-domain=cluster.local"""
-                    )
-            )
+            d.addCallback(lambda ignored: log("Installed worker on %s." % (node['public'],)))
             deferreds.append(d)
 
         return gatherResults(deferreds)
