958ff9d0dadd5993b74c641e9e0da2c20f63753f
Luke Marsden <luke@clusterhq.com>
2015-12-02 12:38:17 +0000

    master ip everywhere

--- unofficial_flocker_tools/kubernetes.py
+++ unofficial_flocker_tools/kubernetes.py
@@ -11,14 +11,14 @@ def install_kubernetes(reactor, configFile):
         d = c.runSSHAsync(
                 c.config["control_node"],
                 # Install kubernetes
-                "K8S_VERSION=1.1.2 sh -c '"
-                    "curl -sSL https://raw.githubusercontent.com/kubernetes/kubernetes"
-                    "/master/docs/getting-started-guides/docker-multinode/master.sh |bash"
-                "' "
-                # Install kubectl on the master for convenience for user
-                "&& curl -sSL https://storage.googleapis.com/kubernetes-release/"
-                "release/v1.1.2/bin/linux/amd64/kubectl -o /usr/local/bin/kubectl && "
-                "chmod +x /usr/local/bin/kubectl"
+                ("K8S_VERSION=1.1.2 MASTER_IP=%(master_ip)s sh -c '"
+                     "curl -sSL https://raw.githubusercontent.com/kubernetes/kubernetes"
+                     "/master/docs/getting-started-guides/docker-multinode/master.sh |bash"
+                 "' "
+                 # Install kubectl on the master for convenience for user
+                 "&& curl -sSL https://storage.googleapis.com/kubernetes-release/"
+                 "release/v1.1.2/bin/linux/amd64/kubectl -o /usr/local/bin/kubectl && "
+                 "chmod +x /usr/local/bin/kubectl") % dict(master_ip=c.config["control_node"],)
         )
         d.addCallback(lambda ignored: log("Installed master."))
         deferreds.append(d)
@@ -27,10 +27,10 @@ def install_kubernetes(reactor, configFile):
             log("Installing Kubernetes worker on %s..." % (node['public'],))
             d = c.runSSHAsync(
                     node['public'],
-                    "K8S_VERSION=1.1.2 MASTER_IP=%(master_ip)s sh -c '"
-                        "curl -sSL https://raw.githubusercontent.com/kubernetes/kubernetes"
-                        "/master/docs/getting-started-guides/docker-multinode/worker.sh |bash"
-                    "' " % dict(master_ip=c.config["control_node"],)
+                    ("K8S_VERSION=1.1.2 MASTER_IP=%(master_ip)s sh -c '"
+                         "curl -sSL https://raw.githubusercontent.com/kubernetes/kubernetes"
+                         "/master/docs/getting-started-guides/docker-multinode/worker.sh |bash"
+                     "' ") % dict(master_ip=c.config["control_node"],)
             )
             d.addCallback(lambda ignored: log("Installed worker on %s." % (node['public'],)))
             deferreds.append(d)
