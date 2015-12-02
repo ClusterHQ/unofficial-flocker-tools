9704a5584ab938f6ecf208e06b78325f3085d4f2
Luke Marsden <luke@clusterhq.com>
2015-12-02 11:09:06 +0000

    Do the other way of specifying env vars that actually works and doesn't try to evaluate the output

--- unofficial_flocker_tools/kubernetes.py
+++ unofficial_flocker_tools/kubernetes.py
@@ -11,10 +11,10 @@ def install_kubernetes(reactor, configFile):
         d = c.runSSHAsync(
                 c.config["control_node"],
                 # Install kubernetes
-                "K8S_VERSION=1.1.2 $("
+                "K8S_VERSION=1.1.2 sh -c '"
                     "curl -sSL https://raw.githubusercontent.com/kubernetes/kubernetes"
                     "/master/docs/getting-started-guides/docker-multinode/master.sh |sh"
-                ") "
+                "' "
                 # Install kubectl on the master for convenience for user
                 "&& curl -sSL https://storage.googleapis.com/kubernetes-release/"
                 "release/v1.1.2/bin/linux/amd64/kubectl -o /usr/local/bin/kubectl && "
@@ -27,10 +27,10 @@ def install_kubernetes(reactor, configFile):
             log("Installing Kubernetes worker on %s..." % (node['public'],))
             d = c.runSSHAsync(
                     node['public'],
-                    "K8S_VERSION=1.1.2 MASTER_IP=%(master_ip)s $("
+                    "K8S_VERSION=1.1.2 MASTER_IP=%(master_ip)s sh -c '"
                         "curl -sSL https://raw.githubusercontent.com/kubernetes/kubernetes"
                         "/master/docs/getting-started-guides/docker-multinode/worker.sh |sh"
-                    ") " % dict(master_ip=c.config["control_node"],)
+                    "' " % dict(master_ip=c.config["control_node"],)
             )
             d.addCallback(lambda ignored: log("Installed worker on %s." % (node['public'],)))
             deferreds.append(d)
