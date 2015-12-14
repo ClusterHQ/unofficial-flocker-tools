9ecf7b3da80257a162c18583b15461f16f2b4400
Luke Marsden <luke@clusterhq.com>
2015-12-14 19:28:34 +0000

    Remove slightly more specific files

--- unofficial_flocker_tools/hatch.py
+++ unofficial_flocker_tools/hatch.py
@@ -399,7 +399,7 @@ class Destroy(Options):
 class Cleanup(Options):
     def run(self):
         # XXX gross, also need to add a warning
-        os.system("rm -rf *.key *.crt cluster.yml agent.yml node_mapping.yml *.txt terraform/")
+        os.system("rm -rf *.key *.crt cluster.yml agent.yml node_mapping.yml install-log.txt terraform/")
 
 commands = {
     "version": Version,
